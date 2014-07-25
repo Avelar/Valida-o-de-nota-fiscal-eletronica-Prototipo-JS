9a6b7aa499ec7f0548ce0cb0f1f0db61bdee12f7
avelar <fortunato.avelar@gmail.com>
2014-07-25 09:01:51 -0300

    set compass configuration

--- sass-tests/sassCompass/config.rb
+++ sass-tests/sassCompass/config.rb
@@ -1,24 +1,37 @@
-# Require any additional compass plugins here.
+# ==============================================================================
+# GENERAL CONFIGURATION
+#
+# View all options here:
+# http://compass-style.org/help/tutorials/configuration-reference/
+# ==============================================================================
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+# Require any additional compass plugins here.
+require "sass-globbing"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Enable relative paths to assets via compass helper functions.
+relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# ==============================================================================
+# COMPASS DIRECTORY CONFIGURATION
+# ==============================================================================
 
+# The root of all operations, must be set for Compass to work.
+http_path             = "/"
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# Directory containing the SASS source files
+sass_dir              = "assets/stylesheets"
+
+# Directory where Compass dumps the generated CSS files
+css_dir               = "public"
+
+# Directory where font files use in font-face declarations are stored
+fonts_dir             = "assets/fonts"
+
+# Directory where Compass stores the Grid image, and the sites images are stored
+images_dir            = "assets/images"
+
+# Directory where the sites' JavaScript file are stored
+javascripts_dir       = "assets/javascripts"
